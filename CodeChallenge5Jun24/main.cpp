a64fec1bf56681fd551d8614d1056350b849db09
kingf <kingf@JMNLAP5>
2024-06-06 07:51:17 -0400

    Add project files.

---
+++ CodeChallenge5Jun24/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdio>
+
+int main()
+{
+    printf("hello from %s!\n", "CodeChallenge5Jun24");
+    return 0;
+}
+/// <summary>
+/// This function waits a randomized interval of time within a predefined range, 
+/// formats a buffer with a number and up to 10 random letters, and then sends
+/// it for "processing"
+/// </summary>
+/// <param name=""></param>
+void emulate_sender(void)
+{
+
+}
